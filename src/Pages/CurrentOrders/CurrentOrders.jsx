4da0fc7c1e1b7dec0ae63beaebb6924720e82eb8
marianalopesdev <marianalopes.dev@gmail.com>
2023-06-01 17:20:02 -0300

    login page added to app.jsx / new pages added

---
+++ src/Pages/CurrentOrders/CurrentOrders.jsx
@@ -0,0 +1,10 @@
+import { Header } from '../../Components/Header/Header'
+
+export function CurrentOrders (){
+    return (
+        <>
+        <Header />
+        <h1>Current Orders</h1>
+        </>
+    )
+}
---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+import { Form } from '../../Components/Form/Form'
+import { Header } from '../../Components/Header/Header'
+
+export function Login(){
+    return (
+        <>
+        <Header />
+        <Form /> 
+        </>
+    )
+}
