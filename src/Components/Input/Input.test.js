5cc9e68aae2ac610c6c9f015ea065e982e4f66f2
marinacezario <marina.cezario@gmail.com>
2023-06-11 18:36:08 -0300

    login test ok

--- src/Components/Input/Input.test.js
+++ src/Components/Input/Input.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, /* fireEvent, */ waitFor } from '@testing-library/react';
 import { Input } from './Input';
 import '@testing-library/jest-dom'; 
+import userEvent from '@testing-library/user-event';
 
 describe('Input component', () => {
   it('renders without crashing', () => {
@@ -15,16 +16,33 @@ describe('Input component', () => {
     const input = screen.getByPlaceholderText('Email');
     expect(input).toBeInTheDocument();
   });
-  it('should call onChange when input value changes', () => {
+  it('should call onChange when input value changes', async () => {
     const onChangeMock = jest.fn();
 
-    render(<Input onChange={onChangeMock} />);
+    render(<Input
+      onChange={onChangeMock}
+      value= 'T'/>);
     const input = screen.getByRole('textbox');
-
-    fireEvent.input(input, { target: { value: 'Test value' } });
-
-    expect(onChangeMock).toHaveBeenCalledTimes(1);
-    expect(onChangeMock).toHaveBeenCalledWith('Test value');
+      userEvent.type(input, 'T');
+    //fireEvent.change(input, { target: { value: 'Test value' } });
+    await waitFor(() => {
+      expect(onChangeMock).toHaveBeenCalledTimes(1);
+      expect(onChangeMock).toHaveBeenCalledWith(expect.objectContaining({ target: { value: 'T' } }));
+    })
   });
 
+  it('should check the input type', async () => {
+    const onChangeMock = jest.fn();
+
+    render(<Input
+      onChange={onChangeMock}
+      value= 'T'/>);
+    const input = screen.getByRole('textbox');
+      userEvent.type(input, 'T');
+    //fireEvent.change(input, { target: { value: 'Test value' } });
+    await waitFor(() => {
+      expect(onChangeMock).toHaveBeenCalledTimes(1);
+      expect(onChangeMock).toHaveBeenCalledWith(expect.objectContaining({ target: { value: 'T' } }));
+    })
+  });
 });
