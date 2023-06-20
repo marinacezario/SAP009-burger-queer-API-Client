d5340116548ee0d72c8f49f577a0f893b264827e
silvertapecoder <eusou_anacarolina@hotmail.com>
2023-06-20 15:05:18 -0300

    send order form

--- src/Components/SendOrder/SendOrder.jsx
+++ src/Components/SendOrder/SendOrder.jsx
@@ -72,6 +72,7 @@ export function SendOrder (){
                    </div> 
                 )}
             </div>
+            <></>
             <Button
                 id="submit-button"
                 type="submit"
