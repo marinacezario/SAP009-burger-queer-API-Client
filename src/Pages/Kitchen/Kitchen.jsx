722eecf2c9a1bb6d95f727de9a6a6acf8895e1b9
marinacezario <marina.cezario@gmail.com>
2023-07-04 15:27:54 -0300

    orders showing

--- src/Pages/Kitchen/Kitchen.jsx
+++ src/Pages/Kitchen/Kitchen.jsx
@@ -14,8 +14,18 @@ export function Kitchen (){
             const ordersData = response.data
             setPendingOrder(ordersData.filter((order) => order.status === 'pending'))
         })
-    }, [pendingOrders])
+    }, []);
    
+    const handleStatusChange = (orderId, newStatus) => {
+        //update the orders status with a new status
+        const updatedOrder = pendingOrders.map((order) => {
+            if (order.id === orderId) {
+                return { ...order, status: newStatus};
+            }
+            return order;
+        });
+        setPendingOrder(updatedOrder);
+    };
 
     return (
         <>
@@ -23,19 +33,23 @@ export function Kitchen (){
             <h1>Pending Orders</h1>
             <section>
                 {pendingOrders.map((order) => (
-                    <Order 
+                    <Order
+                        key={order.id} 
                         clientName={order.client} 
                         date={order.dateEntry} 
                         products={order.products.map((product) => (
-                            <li>
+                            <li key={product.id}>
                                 <p>{product.qty}</p>
                                 <p>{product.name}</p>
                             </li>
                         ))}
-                        titleBtn=
+                        status={order.status}
+                        titleBtn={order.status === 'pending' ? 'preparing' : 'ready'}
+                        onStatusChange={(newStatus) => handleStatusChange(order.id, newStatus)
+                        }
                     />
                 ))}
             </section>
         </>
-    )
+    );
 }
