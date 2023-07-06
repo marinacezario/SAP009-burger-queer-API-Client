52075cde9eadb7c745fd5a855b60d8fb901ee445
silvertapecoder <eusou_anacarolina@hotmail.com>
2023-07-06 15:39:36 -0300

    set pending and preparing orders

--- src/Components/KitchenOrders/KitchenOrders.jsx
+++ src/Components/KitchenOrders/KitchenOrders.jsx
@@ -1,53 +1,76 @@
-import React, { useEffect, useState } from 'react';
-import { getOrders } from "../../api/orders";
+// import React, { useEffect, useState } from 'react';
+// import { getOrders } from "../../api/orders";
 
-import { Order } from '../../Components/Order/Order';
+// import { Order } from '../../Components/Order/Order';
 
-export const KitchenOrders = ({ sectionTitle, orderStatus, newStatus, btnText }) => {
+// export const KitchenOrders = ({ sectionTitle, newStatus, btnText }) => {
 
-    const [pendingOrders, setPendingOrder] = useState([])
+//     //acho que precisam ser arrays diferentes pra pending order e pra preparing
+//     const [pendingOrders, setPendingOrder] = useState([])
+//     const [preparing, setPreparing] = useState([])
 
-    useEffect(() => {
-        getOrders()
-        .then((response) => {
-            const ordersData = response.data
-            const ordersFilter = ordersData.filter((order) => order.status === orderStatus)
-            console.log(ordersFilter)
-            setPendingOrder(ordersFilter)
-        }) 
-    }, []);
+
+//     //fazer um map????
+//     //dois estados: pendingOrders e preparingOrders
+//     //faz um map ou forEach na response(ordersData)
+//     //pra cada order: 
+//     //se order.status for igual a pending, faz um push daquela order pro array pendingOrders
+//     //se for igual a preparing, faz push pra preparingOrders
+//     useEffect(() => {
+//         getOrders()
+//         .then((response) => {
+//             const ordersData = response.data
+
+//             return ordersData.map((order) => {
+//                 if (order.status === 'pending') {
+//                     setPendingOrder([...pendingOrders, order])
+//                 }
+
+//                 if (order.status === 'preparing') {
+//                     setPreparing([...preparing, order])
+//                 }
+//             })
+
+//             // const ordersFilter = ordersData.filter((order) => order.status === orderStatus)
+//             // console.log(ordersFilter)
+//             // setPendingOrder(ordersFilter)
+//         }) 
+//     }, []);
    
-    const handleStatusChange = (orderId, changeStatus) => {
-        //update the orders status with a new status
-        const updatedOrder = pendingOrders.map((order) => {
-            if (order.id === orderId) {
-                return { ...order, status: changeStatus};
-            }
-            return order;
-        });
-        setPendingOrder(updatedOrder);
-    };
+//     const handleStatusChange = (orderId, changeStatus) => {
+//         //update the orders status with a new status
+//         const updatedOrder = pendingOrders.map((order) => {
+//             if (order.id === orderId) {
+//                 return { ...order, status: changeStatus};
+//             }
+//             return order;
+//         });
+//         setPendingOrder(updatedOrder);
+//         console.log('pendingOrder =', pendingOrders)
+//         return pendingOrders
+        
+//     };
 
-    return (
-        <section>
-            <h2>{sectionTitle}</h2>
-            {pendingOrders.map((order) => (
-                <Order
-                    key={order.id} 
-                    clientName={order.client} 
-                    date={order.dateEntry} 
-                    products={order.products.map((product) => (
-                        <li key={product.id}>
-                            <p>{product.qty}</p>
-                            <p>{product.name}</p>
-                        </li>
-                    ))}
-                    status={order.status}
-                    titleBtn={btnText}
-                    onStatusChange={() => handleStatusChange(order.id, newStatus)
-                    }
-                />
-            ))}
-        </section>
-    );
-}
+//     return (
+//         <section>
+//             <h2>{sectionTitle}</h2>
+//             {pendingOrders.map((order) => (
+//                 <Order
+//                     key={order.id} 
+//                     clientName={order.client} 
+//                     date={order.dateEntry} 
+//                     products={order.products.map((product) => (
+//                         <li key={product.id}>
+//                             <p>{product.qty}</p>
+//                             <p>{product.name}</p>
+//                         </li>
+//                     ))}
+//                     status={order.status}
+//                     titleBtn={btnText}
+//                     onStatusChange={() => handleStatusChange(order.id, newStatus)
+//                     }
+//                 />
+//             ))}
+//         </section>
+//     );
+// }
--- src/Pages/Kitchen/Kitchen.jsx
+++ src/Pages/Kitchen/Kitchen.jsx
@@ -1,14 +1,100 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getOrders } from "../../api/orders";
 
 import { Header } from '../../Components/Header/Header';
-import { KitchenOrders } from '../../Components/KitchenOrders/KitchenOrders';
+import { Order } from '../../Components/Order/Order';
+// import { KitchenOrders } from '../../Components/KitchenOrders/KitchenOrders';
 
 export function Kitchen (){
+
+    const [pendingOrders, setPendingOrder] = useState([])
+    const [preparing, setPreparing] = useState([])
+
+    useEffect(() => {
+        getOrders()
+        .then((response) => {
+            const ordersData = response.data
+
+            return ordersData.map((order) => {
+                if (order.status === 'pending') {
+                    setPendingOrder([...pendingOrders, order])
+                }
+
+                if (order.status === 'preparing') {
+                    setPreparing([...preparing, order])
+                }
+            })
+        }) 
+    }, []);
+
+    //preciso que essa funçao atenda tanto pendingOrder quanto preparing
+    //fazer uma confirmação do order.status
+    //se order.status for igual pending
+    //mapear pendingOrder e fazer a mudança
+
+    //mas se for igual a preparing
+    //mapear preparing e fazer a mudança
+
+    const handleStatusChange = (orderStatus, orderId, changeStatus) => {
+        //update the orders status with a new status
+        const updatedOrder = pendingOrders.map((order) => {
+            if (order.id === orderId) {
+                return { ...order, status: changeStatus};
+            }
+            return order;
+        });
+        setPendingOrder(updatedOrder);
+        console.log('pendingOrder =', pendingOrders)
+
+        return pendingOrders
+        
+    };
 
     return (
         <>
             <Header />
-            <KitchenOrders
+            <section>
+                <h2>PENDING ORDERS</h2>
+                    {pendingOrders.map((order) => (
+                        <Order
+                            key={order.id} 
+                            clientName={order.client} 
+                            date={order.dateEntry} 
+                            products={order.products.map((product) => (
+                                <li key={product.id}>
+                                    <p>{product.qty}</p>
+                                    <p>{product.name}</p>
+                                </li>
+                            ))}
+                            status={order.status}
+                            titleBtn="prepare"
+                            onStatusChange={() => handleStatusChange(order.status, order.id, "preparing")
+                            }
+                        />
+                    ))}
+            </section>
+
+            <section>
+                <h2>PREPARING</h2>
+                    {preparing.map((order) => (
+                        <Order
+                            key={order.id} 
+                            clientName={order.client} 
+                            date={order.dateEntry} 
+                            products={order.products.map((product) => (
+                                <li key={product.id}>
+                                    <p>{product.qty}</p>
+                                    <p>{product.name}</p>
+                                </li>
+                            ))}
+                            status={order.status}
+                            titleBtn="ready"
+                            onStatusChange={() => handleStatusChange(order.id, "ready")
+                            }
+                        />
+                    ))}
+            </section>
+            {/* <KitchenOrders
                 sectionTitle="Pending Orders"
                 orderStatus="pending"
                 btnText="prepare"
@@ -19,7 +105,52 @@ export function Kitchen (){
                 orderStatus="preparing"
                 btnText="ready"
                 newStatus="ready"
-            />
+            /> */}
         </>
     );
 }
+
+
+
+// import { Order } from '../../Components/Order/Order';
+
+// export const KitchenOrders = ({ sectionTitle, newStatus, btnText }) => {
+
+   
+//     const handleStatusChange = (orderId, changeStatus) => {
+//         //update the orders status with a new status
+//         const updatedOrder = pendingOrders.map((order) => {
+//             if (order.id === orderId) {
+//                 return { ...order, status: changeStatus};
+//             }
+//             return order;
+//         });
+//         setPendingOrder(updatedOrder);
+//         console.log('pendingOrder =', pendingOrders)
+//         return pendingOrders
+        
+//     };
+
+//     return (
+//         <section>
+//             <h2>{sectionTitle}</h2>
+//             {pendingOrders.map((order) => (
+//                 <Order
+//                     key={order.id} 
+//                     clientName={order.client} 
+//                     date={order.dateEntry} 
+//                     products={order.products.map((product) => (
+//                         <li key={product.id}>
+//                             <p>{product.qty}</p>
+//                             <p>{product.name}</p>
+//                         </li>
+//                     ))}
+//                     status={order.status}
+//                     titleBtn={btnText}
+//                     onStatusChange={() => handleStatusChange(order.id, newStatus)
+//                     }
+//                 />
+//             ))}
+//         </section>
+//     );
+// }
