235f496454469280cb70e706109429db6894dfa8
silvertapecoder <eusou_anacarolina@hotmail.com>
2023-07-11 14:33:26 -0300

    header buttons logic

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,10 +7,12 @@ import logoMobile from '../../assets/img/logo-header-mobile.png';
 import { Button } from '../Button/Button';
 
 const getNavigationDetails = (pathname) => {
-  const route = pathname === '/new-order' ? "/current-orders" : "/new-order"; 
-  const text = pathname === '/new-order' ? "current orders" : "new order";
+  const routeWaiter = pathname === '/new-order' ? "/current-orders" : "/new-order"; 
+  const textWaiter = pathname === '/new-order' ? "current orders" : "new order";
+  const routeAdmin = pathname === '/users' ? "/products" : "/users";
+  const textAdmin = pathname === '/users' ? "products" : "employees";
 
-  return {route, text}
+  return {routeWaiter, textWaiter, routeAdmin, textAdmin}
 }
 
 export function Header({ showButton }) {
@@ -22,9 +24,13 @@ export function Header({ showButton }) {
   const clickHandler = () => {
     const details = getNavigationDetails(location.pathname);
 
-      navigate(details.route);
-      setText(details.text);
-    
+    if (location.pathname === '/new-order' || location.pathname === '/current-orders') {
+      navigate(details.routeWaiter);
+      setText(details.textWaiter);
+    } else if (location.pathname === '/users' || location.pathname === '/products') {
+      navigate(details.routeAdmin);
+      setText(details.textAdmin);
+    }    
   };
 
   return (
