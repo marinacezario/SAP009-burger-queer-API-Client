8aa3220b115864ed318684a6bd14c592cd767853
marinacezario <marina.cezario@gmail.com>
2023-07-11 16:48:43 -0300

    api users functions created

--- src/api/users.js
+++ src/api/users.js
@@ -16,3 +16,54 @@ export const handleSubmitForm = (email, password) => {
     });
 };
 
+export const getUsers = () => {
+  return api
+    .get('/users'
+    )
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      throw error;
+    });
+};
+
+export const createNewUser = (userId, email, password, role) => {
+  return api
+    .post('/users', {
+      id: userId,
+      email: email,
+      password: password,
+      role: role,
+    })
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      throw error;
+    })
+};
+
+export const editUser = (uid) => {
+  return api
+    .patch(`/users/${uid}`
+    )
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      throw error;
+    })
+};
+
+export const deleteUser = (uid) => {
+  return api
+  .delete(`/users/${uid}`
+  )
+  .then((response) => {
+    return response;
+  })
+  .catch((error) => {
+    throw error;
+  })
+};
